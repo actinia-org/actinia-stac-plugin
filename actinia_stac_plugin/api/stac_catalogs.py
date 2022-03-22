2593ca8d38996ba830a9a026243f28ac42fcff43
Jorge Andrés Herrera Maldonado <35478550+joaherrerama@users.noreply.github.com>
2022-03-22 18:17:30 +0100

    Update actinia_stac_plugin/api/stac_catalogs.py
    Co-authored-by: Carmen Tawalika <mmacata@users.noreply.github.com>

--- actinia_stac_plugin/api/stac_catalogs.py
+++ actinia_stac_plugin/api/stac_catalogs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2021 mundialis GmbH & Co. KG
+Copyright (c) 2022 mundialis GmbH & Co. KG
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
