b6a823e5b264be8845571b1e74c1f9d336d1fd0f
Carmen Tawalika <mmacata@users.noreply.github.com>
2024-06-18 18:20:41 +0200

    fix import error (#88)

--- actinia_stac_plugin/__init__.py
+++ actinia_stac_plugin/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import DistributionNotFound, get_distribution
+import importlib.metadata
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "actinia_stac_plugin.wsgi"
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    dist_name = "actinia_metadata_plugin.wsgi"
+    __version__ = importlib.metadata.version(dist_name)
+except Exception:
     __version__ = "unknown"
